e77e09b6cc887cdff56227c0c85013bb4e0e5cfc
walaaAlharthi <alharthiw727@gmail.com>
2025-09-28 21:57:41 +0300

    Initial commit - add Calculator project

---
+++ src/Calc/App.java
@@ -0,0 +1,14 @@
+package Calc;
+
+/**
+ *
+ * @author youcefhmd
+ */
+
+public class App {
+
+    public static void main(String[] args) {
+        Calculator.getInstance().setVisible(true);
+    }
+
+}
